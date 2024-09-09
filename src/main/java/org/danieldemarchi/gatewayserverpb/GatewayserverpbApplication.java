eb11e594c9a2bbccb96e45bc77875aae2cbb155c
Bagelboi <donkeysoda22@gmail.com>
2024-09-08 21:23:11 -0300

    yo

---
+++ src/main/java/org/danieldemarchi/gatewayserverpb/GatewayserverpbApplication.java
@@ -0,0 +1,13 @@
+package org.danieldemarchi.gatewayserverpb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayserverpbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverpbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/danieldemarchi/gatewayserverpb/GatewayserverpbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.danieldemarchi.gatewayserverpb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverpbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
